d19bf7074eedfd9c737b3672c7cdce74bd3fed8f
obijuan <juan@iearobotics.com>
2019-09-17 10:31:02 +0200

    L02: animation, images and example added

---
+++ wiki/L02/download/text.s
@@ -0,0 +1,10 @@
+#-- Probando la directiva .text
+
+	#-- Ensamblador: A partir de aquí deberás situar todo
+	#-- en el segmentos de código
+	.text
+	
+	#-- Todo lo que coloquemos aquí se insertará en el 
+	#-- segmento de código
+	
+	
