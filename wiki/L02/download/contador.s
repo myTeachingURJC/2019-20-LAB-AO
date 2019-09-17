b2e015d0af4ce42a51c00b025a0a36fd5f6cbca5
obijuan <juan@iearobotics.com>
2019-09-17 09:29:31 +0200

    L02: Image added

--- wiki/L02/download/contador.s
+++ wiki/L02/download/contador.s
@@ -6,9 +6,15 @@
 	#-- Inicializar el registro x5 a 0
 	addi x5, x0, 0
 	
+	#-- x6 = 0
+	addi x6, x0, 0
+	
 bucle:
 	#-- Incrementar el registro x5 en una unidad
 	addi x5, x5, 1  #-- x5 = x5 + 1
+	
+	#-- x6 = x6 + 2
+	addi x6, x6, 2
 	
 	#-- Repetir indefinidamente		
 	b bucle
