6a3ac5eb961c339ebedcd1536322c7792ce24755
obijuan <juan@iearobotics.com>
2019-09-24 12:45:50 +0200

    L03: More images added

---
+++ wiki/L03/download/data-03.s
@@ -0,0 +1,15 @@
+#-- Ejemplo de uso de etiquetas en el
+#-- segmento de datos
+
+	.data
+	
+	#-- Almacenamos 5 variables en memoria
+	#-- inicializadas con los valores 1,2,3,4 y 5
+	#-- Cada variable es una palabra, almacenada en una 
+	#-- direccion de memoria (alineada)
+a:	.word 1
+b:	.word 2
+c:	.word 3
+d:	.word 4
+e:	.word 5
+	
