bddfeb01e8b935e5470ceadbf26844cc0ef1ba97
obijuan <juan@iearobotics.com>
2019-09-24 16:46:40 +0200

    L03: Image added

--- wiki/L03/download/segmentos.asm
+++ wiki/L03/download/segmentos.asm
@@ -3,15 +3,14 @@
 #-- El ensamblador asigna las direcciones a las etiquetas
 #-- en función del segmento en el que están
 	
+	#-- Segmento de datos
 	.data
 	
 datos:
-
 	.word 0xCAFEBACA
 	.word 0xFACEB00C
 	
-
-
+	#-- Segmento de Codigo
 	.text
 codigo:
 
