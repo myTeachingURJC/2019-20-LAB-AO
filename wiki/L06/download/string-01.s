7899a166bc4147c85995a77221403a7238df6c74
obijuan <juan@iearobotics.com>
2019-10-14 08:48:07 +0200

    L06: Image added

---
+++ wiki/L06/download/string-01.s
@@ -0,0 +1,10 @@
+#-- Ejemplo de definición de cadenas
+
+	#-- Se definen en el segmento de datos
+	.data
+	
+	#-- Usamos la directiva .string
+	.string "En un lab de la URJC"
+	.string "de cuyo numero no quiero acordarme"
+	
+	
