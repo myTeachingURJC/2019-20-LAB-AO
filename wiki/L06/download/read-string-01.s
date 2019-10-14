54b832b850d53164b5a61385feb3787638f1cd63
obijuan <juan@iearobotics.com>
2019-10-14 11:37:41 +0200

    L06: Image added

--- wiki/L06/download/read-string-01.s
+++ wiki/L06/download/read-string-01.s
@@ -2,11 +2,13 @@
 #-- una cadena definida en tiempo de ejecucion
 
 	#--- Tamano maximo cadena
-	.eqv MAX 16
-
+	.eqv MAX 18
+	
 	#-- Codigo de los servicios del Sistema Operativo
 	.eqv EXIT         10
 	.eqv PRINT_STRING 4
+	.eqv READ_STRING  8	
+
 
 	.data
 	
@@ -14,19 +16,28 @@
 cad1:	.space MAX
 	
 	#-- Cadena definida en tiempo de compilacion
-cad2:   .string "Soy una cadena definida"
-
+cad2:   .string "Nombre?: "
 	
 	.text
 	
-	#------ Imprimir la primera cadena
+	#------ Imprimir mensaje para pedir el nnombre
 	
 	#-- Situar en a0 la direccion de la cadena
-	la a0, cad1
+	la a0, cad2
 	
 	#-- Imprimir la cadena
 	li a7, PRINT_STRING
 	ecall 
+	
+	#--- Llamar al servicio ReadString para pedir el nombre
+	#-- Direccion donde almacenar la cadena del usuario
+	la a0, cad1
+	
+	#-- Longitud máxima de la cadena
+	li a1, MAX
+	
+	li a7, READ_STRING
+	ecall
 	
 	#-- Terminar
 	li a7, EXIT
