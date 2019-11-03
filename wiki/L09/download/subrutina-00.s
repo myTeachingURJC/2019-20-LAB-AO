4e75af12e646827fd5bc22cb50d8ec4a756c31a6
obijuan <juan@iearobotics.com>
2019-11-03 10:47:00 +0100

    L09: Image added

--- wiki/L09/download/subrutina-00.s
+++ wiki/L09/download/subrutina-00.s
@@ -1,23 +1,36 @@
-##-- Un programa que realiza ciertos cálculos
+##-- Esquema de un programa que realiza la tarea 1, 
+#--  entre otras cosas
+#--  Está estructura en UN UNICO PROGRAMA PRINCIPAL
 
 	.include "servicios.asm"
 	
 	.data
 	
-tarea1:	.string "\nTAREA 1\n"
+msg_tarea1:	.string "\nTAREA 1\n"
 	
 	.text 
+	
+	
+	#------------------------ PROGRAMA PRINCIPAL (MAIN)-------------------------
 	#-- PUNTO DE ENTRADA
+	
+	
+	#-- Se realizan las inicializaciones u otras acciones necesarias
+	#-- ....
 	
 	#-- Acción de la Tarea 1: La simulamos imprimiendo un 
 	#-- mensaje en la consola, pero la tarea podría ser cualquiera
 	#-- que necesitasemos en nuestro proyecto
-	la a0, tarea1
-	
+	la a0, msg_tarea1
 	li a7, PRINT_STRING
 	ecall
 	
+	#-- ....
+	#-- Por aqui habria más código
+	#-- ...
 	
 	#-- PUNTO DE SALIDA
 	li a7, EXIT
 	ecall
+	#-----------------------------------------------------------------------
+	
