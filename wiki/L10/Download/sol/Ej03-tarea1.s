5884ab387b2795d448c45acff35c0ae9d8a7d5f5
obijuan <juan@iearobotics.com>
2019-11-15 17:27:39 +0100

    L10: Solutions updated

--- wiki/L10/Download/sol/Ej03-tarea1.s
+++ wiki/L10/Download/sol/Ej03-tarea1.s
@@ -18,12 +18,31 @@ msg:	.string "  Tarea 1\n"
 #-------- Punto de entrada
 tarea1:
 
+	#-- Reservar memoria en la pila
+	addi sp,sp, -16
+	
+	#-- Guardar direccion de retorno
+	sw ra, 12(sp)
 	
 	#-- Imprimir el mensaje
 	la a0, msg
 	li a7, PRINT_STRING
 	ecall
 	
+	#-- Llamar a la tarea 2
+	jal tarea2
+	
+	#-- Imprimir el mensaje
+	la a0, msg
+	li a7, PRINT_STRING
+	ecall
+	
+	
+	##-- Recuperar dirección de retorno
+	lw ra, 12(sp)
+	
+	#-- Liberar memoria de la pila
+	addi sp, sp, 16		
 			
 #------- Punto de salida
 	ret
