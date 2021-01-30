abcaa25ccf843e1d5192b5500e400eb88d584024
Obijuan <obijuan.cube@gmail.com>
2021-01-30 08:24:23 +0100

    automatic test updated

--- wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-01.sh
+++ wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-01.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 echo "-----Ejecutando TEST-01-----"
 
+##-- Nombre del elemento a probar
+ITEM="max.s"
+
+echo ""
+echo "PROBANDO: "$ITEM
+echo ""
+
 ##-- Comando a ejecutar
-cmd="java -jar rars1_5.jar SOL_test_max.s max.s"
+cmd="java -jar rars1_5.jar SOL_test_max.s ../$ITEM"
 
 ##-- Mostrar comando en consola y ejecutarlo
 echo $cmd
