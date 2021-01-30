1665d129002d2457a1dc32a7ae1ad854fd7b9bef
Obijuan <obijuan.cube@gmail.com>
2021-01-30 13:46:08 +0100

    Test updated

--- wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-02.sh
+++ wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-02.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "-----Ejecutando TEST-02-----"
 
+##-- Numero de pasos maximos
+##-- Es para detectar bucles infinitos
+MAX_STEPS=10000
+
 ##-- Nombre del elemento a probar
 ITEM="test_max.s"
 
@@ -9,7 +13,11 @@ echo "PROBANDO: "$ITEM
 echo ""
 
 ##-- Comando a ejecutar
-cmd="java -jar rars1_5.jar ../$ITEM SOL_max.s"
+##-- Parametro ic: Muestra al final el numero de instrucciones
+##--   ejecutadas
+##-- Se para tambien como parametro el numero maximo de 
+##-- pasos
+cmd="java -jar rars1_5.jar ic $MAX_STEPS ../$ITEM SOL_max.s"
 
 ##-- Mostrar comando en consola y ejecutarlo
 echo $cmd
