b358be6649ccb9912cf29d230b6a3a05da146ed2
Obijuan <obijuan.cube@gmail.com>
2021-01-30 11:52:19 +0100

    Test1 updated

--- wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-01.sh
+++ wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-01.sh
@@ -9,15 +9,10 @@ echo "PROBANDO: "$ITEM
 echo ""
 
 ##-- Comando a ejecutar
-cmd="java -jar rars1_5.jar SOL_test_max.s ../$ITEM"
+cmd="java -jar rars1_5.jar TEST_max.s ../$ITEM"
 
 ##-- Mostrar comando en consola y ejecutarlo
 echo $cmd
 $cmd
 
-##-- Mostrar cual deberia ser la salida correcta
-echo ""
-echo "*** SALIDA CORRECTA: ***"
-echo ""
-cat TEST-01-output.txt
 read -p "------FIN-------"
