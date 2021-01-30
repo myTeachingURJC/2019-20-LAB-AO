184d807d74a17a665cd4158257836ada07f2c6e1
Obijuan <obijuan.cube@gmail.com>
2021-01-30 12:44:17 +0100

    Test updated

--- wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-04.sh
+++ wiki/Examenes/2021-01-21-Examen-practicas-ASA2/solucion/exec-test/TEST-04.sh
@@ -9,16 +9,10 @@ echo "PROBANDO: "$ITEM
 echo ""
 
 ##-- Comando a ejecutar
-cmd="java -jar rars1_5.jar SOL_test_peso.s ../$ITEM"
+cmd="java -jar rars1_5.jar TEST_peso.s ../$ITEM"
 
 ##-- Mostrar comando en consola y ejecutarlo
 echo $cmd
 $cmd
-
-##-- Mostrar cual deberia ser la salida correcta
-echo ""
-echo "*** SALIDA CORRECTA: ***"
-echo ""
-cat TEST-04-output.txt
 
 read -p "------FIN-------"
