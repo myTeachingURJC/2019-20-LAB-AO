caba4be3493313f9be9424064be2542664c2272a
Obijuan <obijuan.cube@gmail.com>
2023-11-18 09:41:06 +0100

    Add TELECO P2 exam (2023-11-17)

---
+++ wiki/Examenes/2023-11-17-P2-Teleco/randhex.s
@@ -0,0 +1,61 @@
+#-- Programa para imprimir numeros aleatorios
+
+	#-- Servicios del sistema operativo (ESPECIFICACION)
+	.include "sis.s"
+
+	.data
+num:    .word 0  #-- ESPECIFICACION: Numero introducido por el usuario
+msg1:	.string "Cantidad de numeros aleatorios: " #-- ESPECIFICACION
+
+	.text
+	
+	#-- Imprimir mensaje
+	la a0, msg1
+	li a7, PRINT_STRING
+	ecall
+	
+	#-- Pedir cantidad de numeros aleatorios #-- ESPECIFICACION
+	li a7, READ_INT
+	ecall
+	
+	#-- t0: Cantidad de numeros aleatorios
+	mv t0,a0
+	
+	#-- BONUS! Guardar este numero al comienzo del segmento de datos
+	la t1,num
+	sw t0,0(t1)
+	
+	#-- Bucle principal
+bucle:
+	#-- Si el numero es 0, terminar
+	beq t0, zero, fin
+	
+	#-- Obtener numero aleatorio, llamando al sistema operativo
+	#-- (ESPECIFICACION)
+	li a0,1
+	li a7, RAND_INT
+	ecall
+	
+	#-- Imprimir numero aleatorio, en hexadecimal (ESPECIFICACION)
+	li a7, PRINT_INT_HEX
+	ecall
+	
+	#-- Imprimir salto de linea (ESPECIFICACION)
+	li a0, '\n'
+	li a7, PRINT_CHAR
+	ecall
+	
+	#-- Decrementar t0
+	addi t0,t0,-1
+	
+	#-- Repetir
+	b bucle
+	
+fin:
+	#-- Terminar
+	li a7, EXIT
+	ecall
+	
+	
+	
+	
---
+++ wiki/Examenes/2023-11-17-P2-Teleco/sis.s
@@ -0,0 +1,13 @@
+#-- Código de los servicios del sistema operativo
+#-- Incluir estos archivos en tus programas
+#-- para acceder a ellos fácilmente, y hacerlos más
+#-- legibles
+
+	.eqv PRINT_STRING  4
+	.eqv READ_INT      5
+	.eqv PRINT_INT     1
+	.eqv PRINT_INT_HEX 34
+	.eqv RAND_INT      41
+	.eqv PRINT_CHAR    11
+	.eqv EXIT          10
+	
