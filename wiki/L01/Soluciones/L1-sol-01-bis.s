6fa589d62fed0687208980f40389cfc8109bfdb8
obijuan <obijuan.cube@gmail.com>
2025-09-24 10:40:23 +0200

    L1: update solution for Exercise 1

---
+++ wiki/L01/Soluciones/L1-sol-01-bis.s
@@ -0,0 +1,23 @@
+# Sesion L1 
+# Solucion al ejercicio 1 (avanzada)
+#
+#-- Asignar los siguientes valores a los registro indicados:
+#-- x3=3, x4=4, x5=5, x6=6, x7=7 y x8=8
+#--
+#-- La operación de asignacion se realiza tipicamente con
+#-- la pseudo-instruccion li, que no vemos hasta la sesion L3
+#-- El programa es equivalente, pero usando li en vez de addi
+#-- (pero genera exactamente las mismas instrucciones basicas)
+
+	.text
+
+	li x3, 3   # x3 = 3
+ 	li x4, 4   # x4 = 4
+ 	li x5, 5   # x5 = 5
+ 	li x6, 6   # x6 = 6
+ 	li x7, 7   # x7 = 7
+ 	li x8, 8   # x8 = 8
+ 	
+	#-- Terminar
+	li a7, 10
+	ecall
--- wiki/L01/Soluciones/L1-sol-01.s
+++ wiki/L01/Soluciones/L1-sol-01.s
@@ -1,16 +1,18 @@
-# Solucion a la actividad no guiada 1
+# Sesion L1 
+# Solucion al ejercicio 1
+#
 #-- Asignar los siguientes valores a los registro indicados:
 #-- x3=3, x4=4, x5=5, x6=6, x7=7 y x8=8
 
 	.text
-	
+
 	addi x3, x0, 3   # x3 = 3
 	addi x4, x0, 4   # x4 = 4
 	addi x5, x0, 5   # x5 = 5
 	addi x6, x0, 6   # x6 = 6
 	addi x7, x0, 7   # x7 = 7
 	addi x8, x0, 8   # x8 = 8
-	
+
 	#-- Terminar
 	li a7, 10
 	ecall
