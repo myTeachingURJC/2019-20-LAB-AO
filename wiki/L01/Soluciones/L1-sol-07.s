0d81bde44388af061da797843b9fa28bd6abbf0b
obijuan <obijuan.cube@gmail.com>
2025-09-25 13:38:09 +0200

    L1: solutions: add animation

--- wiki/L01/Soluciones/L1-sol-07.s
+++ wiki/L01/Soluciones/L1-sol-07.s
@@ -4,16 +4,20 @@
 # Consigue que se pueda ejecutar paso a paso
 # -- Este codigo tiene todos los errores solucionados
 
-	.text
-	
+        .text
+
 	addi x17, x0, 10
 	addi x12, x0, 8
 	addi x7, x0, 0
 a:
-	beq x17,x0,fin
+	beq x17,x0,fin  #-- Comprueba terminacion
 	add x7, x7, x12
 	addi x17, x17, -1
+	
+	#-- repetir 
+	
 	j a
+	
 fin:
 	li a7, 10
 	ecall
