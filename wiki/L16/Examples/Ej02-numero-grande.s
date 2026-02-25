af9e185f6a57683396529007f2ede267a342ba22
obijuan <obijuan.cube@gmail.com>
2026-02-25 09:27:14 +0100

    L16: Add image

---
+++ wiki/L16/Examples/Ej02-numero-grande.s
@@ -0,0 +1,30 @@
+#-- Ejemplo de prueba de multiplicacion
+#-- de un numero grande
+
+	.include "so.h"
+	.include "stdio.h"
+	
+	.text
+	
+	#-- Numeros a multiplicar
+	li s0, 0x10000
+	li s1, 0xFFFF
+	
+	#-- Imprimir operacion a realizar
+	PRINT_INT_HEXR(s0)
+	PRINT_STRINGI(" * ")
+	PRINT_INT_HEXR(s1)
+	PRINT_STRINGI(" = ")
+	
+	#-- Realizar la multiplicacion
+	mv a0, s0
+	mv a1, s1
+	jal mul_basic
+	mv t0, a0
+	
+	#-- Imprmir resultado
+	PRINT_INT_HEXR(t0)
+	PRINT_CHARI('\n')
+	
+	EXIT
+	
